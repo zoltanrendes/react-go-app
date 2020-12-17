6e689a2747e48b6a3a3ce9e91ee082e121c9e2e7
Zoltan Rendes <rendes.zoltan@gmail.com>
2020-12-17 21:56:27 +0000

    Security fix latest

--- ui/src/App.test.js
+++ ui/src/App.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { mount, shallow } from "enzyme";
+import { mount } from "enzyme";
 import { act } from "react-dom/test-utils";
 
 import App from "./App";
@@ -10,31 +10,31 @@ const mock = new MockAdapter(axios);
 
 mock.onGet("/main").reply(200, { text: "Welcome" });
 
-async function updateWrapper(wrapper, amount = 0) {
-    await act(async () => {
-        wrapper.update();
-    });
-}
+const updateWrapper = async (wrapper, amount = 0) => {
+  await act(async () => {
+    wrapper.update();
+  });
+};
 
 describe("App.js", () => {
-    beforeAll(() => jest.spyOn(React, "useEffect").mockImplementation(React.useLayoutEffect));
-    afterAll(() => React.useEffect.mockRestore());
-    it("it matches snapshot", async () => {
-        await act(async () => {
-            const wrapper = mount(<App />);
-            expect(wrapper).toMatchSnapshot();
-        });
+  beforeAll(() => jest.spyOn(React, "useEffect").mockImplementation(React.useLayoutEffect));
+  afterAll(() => React.useEffect.mockRestore());
+  it("it matches snapshot", async () => {
+    await act(async () => {
+      const wrapper = mount(<App />);
+      expect(wrapper).toMatchSnapshot();
+    });
+  });
+
+  it("shows welcome", async () => {
+    let wrapper;
+
+    await act(async () => {
+      wrapper = mount(<App />);
     });
 
-    it("shows welcome", async () => {
-        let wrapper;
-
-        await act(async () => {
-            wrapper = mount(<App />);
-        });
-
-        const linkElement = wrapper.find("p");
-        wrapper.update();
-        expect(linkElement.text()).toBe("Hello, Welcome!");
-    });
+    const linkElement = wrapper.find("p");
+    updateWrapper(wrapper);
+    expect(linkElement.text()).toBe("Hello, Welcome!");
+  });
 });
